dca80fc8441be42799cd226ba52e906c1e52e349
Parth <parthfulpagar@gmail.com>
2024-04-18 00:57:27 +0530

    day2 19 20 .py added

--- Day2/19.py
+++ Day2/19.py
@@ -2,3 +2,14 @@
 # generate numbers for the following series and find its addition
 # [9 + 99 + 999 + 9999+……..]
 
+user_num = int(input("Enter a num - "))
+sum = 0
+
+print("[",end="")
+for n in range(user_num):
+    print("9"*(n+1),end=" + ")
+    sum += (int("9"*(n+1)))
+print("]",end="")
+
+print("")
+print(sum)
