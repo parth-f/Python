ad929926a81a083ec8163c1e05663937238a74b6
parth f <parthfulpagar@gmail.com>
2024-04-24 01:15:26 +0530

    Dict new code Q2

--- D6 Tuple Set Dict/D2.py
+++ D6 Tuple Set Dict/D2.py
@@ -1,20 +1,58 @@
 # 2. Given a dictionary of students and their favourite colours:
-# people={'Arham':'Blue','Lisa':'Yellow',''Vinod:'Purple','Jenny':'Pink'}
-# a. Find out how many students are in the list
-# b. Change Lisa’s favourite colour
-# c. Remove 'Jenny' and her favourite colour
+people={'Arham':'Blue','Lisa':'Yellow','Vinod':'Purple','Jenny':'Pink'}
 
-l={'Arham':'Blue','Lisa':'Yellow','Vinod':'Purple','Jenny':'Pink'}
-print(l.keys())
-x = list(l.keys())
-print(len(x))
-l['Lisa']='green'
-print(l)
-del l['Jenny']
-print(l)
-y="".join(l)
+## a. Find out how many students are in the list
+print("Num of students =", len( list(people.keys()) ) )
 
-print(l)
+# output > Num of students = 4
+
+## b. Change Lisa’s favourite colour
+people['Lisa']='Green'
+print("Lisa fav Color is",people['Lisa'])
+
+# output > Lisa fav Color is Green
+
+## c. Remove 'Jenny' and her favourite colour
+del people['Jenny']
+print(people)
+
+# output > {'Arham': 'Blue', 'Lisa': 'Green', 'Vinod': 'Purple'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# l={'Arham':'Blue','Lisa':'Yellow','Vinod':'Purple','Jenny':'Pink'}
+# print(l.keys())
+# x = list(l.keys())
+# print(len(x))
+# l['Lisa']='green'
+# print(l)
+# del l['Jenny']
+# print(l)
+# y="".join(l)
+
+# print(l)
 
 
 
