749a975fb655101e47a81edaf03f9dcd55f675ae
parth f <parthfulpagar@gmail.com>
2024-04-24 04:38:13 +0530

    Day 5 Assign Questions of Strings

---
+++ D5 Strings/6.py
@@ -0,0 +1,5 @@
+# 6. Find all occurrences of “USA” from right to left in a given string 
+# ignoring the case. also display the dstarting position
+# Given:
+# str1 = "Welcome to USA. usa awesome, isn't it?
+# Expected answer : 16, 11
---
+++ D6 Tuple Set Dict/D3.py
@@ -0,0 +1,10 @@
+# 3. Sort and print students and their favourite colours alphabetically by name
+
+people={'Arham':'Blue','Lisa':'Green','Vinod':'Purple','Jenny':'Pink'}
+
+sort_ord = sorted(people)
+
+for arrange in people:
+    people.append(["new":2])
+
+print(people)
