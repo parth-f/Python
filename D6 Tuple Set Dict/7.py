4420bfdc4e240a5eabe928d93fdf62be50f0f221
parth f <parthfulpagar@gmail.com>
2024-04-23 22:53:26 +0530

    Commit D7 Que 7 Tuple

---
+++ D6 Tuple Set Dict/7.py
@@ -0,0 +1,3 @@
+# 7. Take two integer values in a & b. Swap their values using tuple, using temparary variable and without tuple and without temparary variable.
+# Ex. a=10 b=23
+# After swapping a=23 b=10
