6192449c9a77710b2df4b7a4e4f49519b8f21867
parth f <parthfulpagar@gmail.com>
2024-04-28 11:04:28 +0530

    practice is back up again

--- D6 Tuple Set Dict/D3.py
+++ D6 Tuple Set Dict/D3.py
@@ -1,10 +1,18 @@
 # 3. Sort and print students and their favourite colours alphabetically by name
 
 people={'Arham':'Blue','Lisa':'Green','Vinod':'Purple','Jenny':'Pink'}
+print("Unsorted list - ",people)
 
-sort_ord = sorted(people)
+sorted_people = sorted(list(people.keys()))
+people2 = dict()
 
-for arrange in people:
-    people.append(["new":2])
+for p in sorted_people:
+    for keyy,vall in people.items(): 
+        if p == keyy:
+            people2[keyy] = vall
+            
+people = people2
+print("Sorted list   - ",people)
 
-print(people)
+# Unsorted list -  {'Arham': 'Blue', 'Lisa': 'Green', 'Vinod': 'Purple', 'Jenny': 'Pink'}
+# Sorted list   -  {'Arham': 'Blue', 'Jenny': 'Pink', 'Lisa': 'Green', 'Vinod': 'Purple'}
