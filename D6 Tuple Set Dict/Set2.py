f3151369cb9768e80b8ec82a8eb353081656a95f
parth f <parthfulpagar@gmail.com>
2024-04-30 01:15:41 +0530

    Dict practice

--- D6 Tuple Set Dict/Set2.py
+++ D6 Tuple Set Dict/Set2.py
@@ -2,3 +2,22 @@
 # set1 = {10, 20, 30, 40, 50}
 # set2 = {30, 40, 50, 60, 70}
 
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+# 1st solution ----------------
+
+# for n in set1:
+#     for m in set2:
+#         if n == m:
+#             print(n)
+
+# 2nd Solution -----------------
+
+# common_elements = set1.intersection(set2)
+# print("Common elements:", common_elements)
+
+# 3rd Solutino -----------------
+
+common_elements = set1 & set2
+print(common_elements)
---
+++ D6 Tuple Set Dict/Set4.py
@@ -0,0 +1,5 @@
+# 4. set of all elements in either A or B, but not both
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+# Expected output:
+# {20, 70, 10, 60}
