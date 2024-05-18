23dea16e6c93e62f510df0fa4ff68fa7f06490cd
parth f <parthfulpagar@gmail.com>
2024-05-18 23:54:52 +0530

    I am back, and here to stay

--- Assignments CDAC Sunbeam/Assign 4 Loops 2/4.py
+++ Assignments CDAC Sunbeam/Assign 4 Loops 2/4.py
@@ -1,14 +1,26 @@
 # 4). Write a method in python to display the elements of list 
-# thrice if it is a number and display the element terminated 
-# with ‘#’ if it is not a number. 
-# Hint-: Use InBuilt Function isdigit() 
-# Refer below as a input:mylist = [’41’,’DROND’,’Sunbeam’, ’13’,’ZARA’]
+#     thrice if it is a number and display the element terminated 
+#     with ‘#’ if it is not a number. 
+#     Hint-: Use InBuilt Function isdigit() 
+#     Refer below as a input:mylist = [’41’,’DROND’,’Sunbeam’, ’13’,’ZARA’]
 
-mylist = ['41','DROND','Sunbeam', '13','ZARA']
-for i,v in enumerate(mylist):
-    maxlen  = 0
-    x = None
-    if len(v)>maxlen:
-        maxlen=len(v)
-        x = i
-print( x)
+mylist = ["41","DROND","Sunbeam", "13","ZARA"]
+
+
+
+def  newDSprint(l :list):  
+    x = len(l)  
+    newl = []
+    for n in l:
+
+        if n.isdigit():
+            newl.append(f"{n}#")
+            print()
+        else:
+            newl.append(f"{n*3}")
+    del l
+    l = newl
+    del newl
+    print(l)
+
+newDSprint(mylist)
